194ea42a9f7ac96f90bffa9bae8de9e53b172c90
MiniPaul <minipaul1227@gmail.com>
2024-02-09 14:04:59 +0530

    user bolg signin branch created

--- controller/UserblogRouter.js
+++ controller/UserblogRouter.js
@@ -15,20 +15,59 @@ router.post("/add",async(req,res)=>{
     let{data}={"data":req.body}
     let password=data.password
 
-    hashPasswordGenerator(password).then(
-        (hashedpassword)=>{
-            console.log(hashedpassword)
-            data.password=hashedpassword
-            console.log(data)
-            let user=new userblogModel(data)
-            let result=user.save()
-            res.json({
-                status:"success"
-            })
-        }
-    )
-    
-    
+    // hashPasswordGenerator(password).then(
+    //     (hashedpassword)=>{
+    //         console.log(hashedpassword)
+    //         data.password=hashedpassword
+    //         console.log(data)
+    //         let user=new userblogModel(data)
+    //         let result=user.save()
+    //         res.json({
+    //             status:"success"
+    //         })
+    //     }
+    // )
+
+    const hashedpassword=await hashPasswordGenerator(password)
+    data.password=hashedpassword
+    let user=new userblogModel(data)
+    let result=await user.save()
+    res.json({
+        status:"success"
+    })
+})
+
+router.post("/signin",async(req,res)=>{
+    let input=req.body
+    let email=req.body.email
+    let data=await userblogModel.findOne({"email":email})
+    if(!data)
+    {
+        return res.json(
+            {
+                status:"invalid user"
+            }
+        )
+    }
+    console.log(data)
+    let dbpassword=data.password
+    let inputpassword=req.body.password
+    console.log(dbpassword)
+    console.log(inputpassword)
+
+    const match=await bcrypt.compare(inputpassword,dbpassword)
+    if(!match)
+    {
+        return res.json(
+            {
+                status:"invalid password"
+            }
+        )
+    }
+
+    res.json({
+        status:"success"
+    })
 })
 
 
